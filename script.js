30d842ba7d522ac8cd3aed55608e82676a91f9a7
Yura Tashkinov <net901@bk.ru>
2021-03-14 21:49:52 +0300

    vrodesdelal

---
+++ script.js
@@ -0,0 +1,40 @@
+let popup = document.querySelector('.popup');
+let nameInput = document.getElementById('name');
+let jobInput = document.getElementById('job');
+let nameform = document.getElementById('nameform')
+let jobform = document.getElementById('jobform')
+
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    nameform.value = nameInput.textContent;
+    jobform.value = jobInput.textContent;
+}
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+let buttonEdit = document.querySelector('.profile-info__button-edit');
+
+// кнопка открытия+ вставка текста с сайта
+buttonEdit.addEventListener('click', function () {
+    openPopup();
+})
+
+let buttonClose = document.querySelector('.popup__close-button');
+
+// кнопка закрытия, без сохранения изменений
+buttonClose.addEventListener('click', function () {
+    closePopup();
+})
+
+let buttonSave = document.querySelector('.popup__save-button');
+
+// кнопка сохранить и закрыть окно
+buttonSave.addEventListener('click', function () {
+    nameInput.textContent = nameform.value;
+    jobInput.textContent = jobform.value;
+    closePopup();
+})
+
